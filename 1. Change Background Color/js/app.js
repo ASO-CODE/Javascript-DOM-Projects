eae92e3fdec9c5a922f5f44a2f7df0c298afcf80
ASO-CODE <60587094+ASO-CODE@users.noreply.github.com>
2022-07-31 04:49:02 +0100

    first commit

---
+++ 1. Change Background Color/js/app.js
@@ -0,0 +1,24 @@
+const colors = ['blue', 'yellow', 'green', '#f15025', 'rgba(125,125,125,0.5)'];
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function () {
+    let random = Math.floor(Math.random() * colors.length);
+    const body = document.body;
+    body.style.backgroundColor = colors[random]
+    console.log(random);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 5. Counter Project/js/app.js
@@ -0,0 +1,30 @@
+// Immediately invoked function expression
+(function () {
+
+    let counterValue = 0;
+
+    const buttons = document.querySelectorAll('.counterBtn');
+    const counter = document.getElementById('counter');
+
+    buttons.forEach(function(btn) {
+        btn.addEventListener('click', function (event) {
+            const value = event.target;
+
+            if (value.classList.contains('prevBtn')) {
+                counterValue--;
+            } else if (value.classList.contains('nextBtn')) {
+                counterValue++;
+            }
+            counter.textContent = counterValue;
+
+            if (counterValue === 0) {
+                counter.style.color = '#333333';
+            } else if (counterValue < 0) {
+                counter.style.color = '#f6511d';
+            } else if (counterValue > 0) {
+                counter.style.color = "#7fb800";
+            }
+        })
+    })
+
+})();
